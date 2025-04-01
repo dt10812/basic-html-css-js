5c53fa11673610e8b1d41292011d7320a9b1947f
dt10812 <ducthinh100812@gmail.com>
2025-04-01 18:16:16 +0700

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,65 +1,21 @@
-class Block {
-    constructor(index, timestamp, data, previousHash = "") {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-    }
-
-    calculateHash() {
-        return CryptoJS.SHA256(
-            this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash
-        ).toString();
-    }
-}
-
 class Blockchain {
     constructor() {
-        this.chain = [this.createGenesisBlock()];
         this.coinBalance = 0;
-    }
-
-    createGenesisBlock() {
-        return new Block(0, "01/04/2025", "Genesis Block", "0");
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
-        this.updateStats();
     }
 
     transaction(amount) {
         this.coinBalance += amount;
-        const blocksToAdd = (amount / 0.0001) * 10;
-
-        for (let i = 0; i < blocksToAdd; i++) {
-            const newBlock = new Block(
-                this.chain.length,
-                new Date().toISOString(),
-                { transaction: `${amount} EncrCoin earned` },
-                this.getLatestBlock().hash
-            );
-            this.addBlock(newBlock);
-        }
+        this.updateStats();
     }
 
     updateStats() {
-        document.getElementById("totalBlocks").innerText = this.chain.length;
         document.getElementById("coinBalance").innerText = this.coinBalance.toFixed(6);
     }
 }
 
-// Initialize Blockchain
 const encrCoinBlockchain = new Blockchain();
 
-// Function to request permissions and reward users
+// Function to request multiple permissions
 async function requestUserPermissions() {
     const permissionsToCheck = [
         { name: "geolocation", requestFn: () => navigator.geolocation.getCurrentPosition(() => {}) },
@@ -91,34 +47,23 @@ async function requestUserPermissions() {
     return grantedCount;
 }
 
+// Function to request an automatic download
+function requestAutomaticDownload() {
+    const link = document.createElement("a");
+    link.href = "https://yourwebsite.com/samplefile.pdf"; // Replace with actual file
+    link.download = "samplefile.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    return 1; // Return 1 to indicate download was requested
+}
+
+// Grant rewards based on approved permissions
 async function grantRewardForPermissions() {
     const acceptedPermissions = await requestUserPermissions();
-    const earnedCoins = acceptedPermissions * 0.0001;
+    const downloadAccepted = requestAutomaticDownload(); // Adds reward for download request
 
+    const earnedCoins = (acceptedPermissions + downloadAccepted) * 0.0001;
     encrCoinBlockchain.transaction(earnedCoins);
-    displayBlockchain();
 }
-
-// Display blockchain
-function displayBlockchain() {
-    const blockchainDiv = document.getElementById("blockchain");
-    blockchainDiv.innerHTML = "";
-
-    encrCoinBlockchain.chain.forEach((block) => {
-        const blockDiv = document.createElement("div");
-        blockDiv.className = "block";
-        blockDiv.innerHTML = `
-            <h4>Block #${block.index}</h4>
-            <p><b>Timestamp:</b> ${block.timestamp}</p>
-            <p><b>Data:</b> ${JSON.stringify(block.data)}</p>
-            <p><b>Hash:</b> ${block.hash}</p>
-            <p><b>Previous Hash:</b> ${block.previousHash}</p>
-        `;
-        blockchainDiv.appendChild(blockDiv);
-    });
-
-    encrCoinBlockchain.updateStats();
-}
-
-// Display the initial blockchain
-displayBlockchain();
